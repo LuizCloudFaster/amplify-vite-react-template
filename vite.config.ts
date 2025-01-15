988657b0bcfd02e69c2084e3029b8ed893111517
Luiz Otávio <luiz.otavio.haubert@cloudfaster.com.br>
2025-01-15 11:22:45 -0300

    added authenticator

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '../amplify_outputs.json': './amplify_outputs.json', // Corrige o alias
+    },
+  },
+  server: {
+    fs: {
+      allow: ["."], // Permite acessar arquivos na raiz do projeto
+    },
+  },
 })
